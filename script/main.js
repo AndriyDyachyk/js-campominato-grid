026d1db43023c85343a495d0257920b6f6118f22
Andriy Dyachyk <andriy.dyachyk98@gmail.com>
2023-05-29 17:39:54 +0200

    Finito esercizio base

--- script/main.js
+++ script/main.js
@@ -1,1 +1,30 @@
 console.log('principi.js loaded');
+
+const button = document.getElementById('play');
+const grid = document.getElementById('grid');
+
+
+button.addEventListener('click', function(){
+    grid_generation();
+})
+
+function grid_generation() {
+    for(let i = 0; i < 100; i++){
+        const square = createSquareElement(i)
+
+        square.addEventListener('click', function (){
+            this.classList.add('clicked');
+            console.log(square.innerText)
+        })
+        grid.append(square);
+    }
+}
+
+function createSquareElement(i){
+    const square = document.createElement('div');
+
+    square.classList.add('square');
+    square.innerText = i + 1;
+
+    return square
+}
